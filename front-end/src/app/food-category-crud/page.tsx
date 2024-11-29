00488f997882c71562e3e5f4a2c60aef0b4a9492
Okibuyan <boika.dashdeleg@gmail.com>
2024-11-29 18:17:30 +0800

    joohon yum hiiv

---
+++ front-end/src/app/food-category-crud/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+  return (
+    <div className="flex w-full">
+      <div className="w-2/6 rounded-box grid h-full ">
+        <div>Food menu</div>
+      </div>
+      <div className="divider divider-horizontal"></div>
+      <div
+        className="bg-[#F7F7F8] w-4/6 grid h-full border 
+      "
+      >
+        content
+      </div>
+    </div>
+  );
+}
